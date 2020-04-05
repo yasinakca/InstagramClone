0f79f69bfde6bbc336ee47a2cba6d9e9469136e1
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-04-05 12:52:26 +0300

    first commit

---
+++ InstagramClone/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by YASIN AKCA on 5.04.2020.
+//  Copyright © 2020 YASIN AKCA. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
